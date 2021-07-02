7459c920fa99fb1d0363ffe0e81d1a9b21eab51f
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-07-02 17:14:04 +0530

    First release!

---
+++ README.md
@@ -0,0 +1,40 @@
+# Minecraft Bedrock Server Exposer (Windows 10+)
+aka libWin10MinecraftTunnel (tho not necessarily a library but a simple script :D)
+> Just a simple script to expose global minecraft servers to your lan tab, Basically it could allow non microsoft/xbox login play in minecraft if target offline server has *Online Mode* turned off (Or you can say server allows cracked/non authenticated clients)! Handy tool when you quickly wana expose your server and ask your friends to play and there's authenticity in you, Or if you want to create a GUI which acts as lobby to invite people and play with them! :D
+
+This is Java Ported Library of [this repository](https://github.com/hari01584/libMcServerProxy) which was used to do the same thing but for Android Bedrock versions of minecraft. It works 
+
+## Usage / How To
+Umn download the latest running executable from [releases](/releases/latest) or you can alternatively build the script from source as well (See how_to_build.txt)
+
+Execute the executable in this format:
+> **bedrockProxy.exe <Target Server IPv4 Address> <Target Server Port>**
+
+Where <Target Server IPv4 Address> referes to ipv4 server address, please note that *domain name like (play.cubicgames.com) will not work here and will give error on starting*, Its better you use webtools like [mcserverstatus](https://mcsrvstat.us/) to ping your target server and get its ipv4 equivalent address :)!
+
+After executing it in the order if no error comes then open your Minecraft Bedrock and go to the LAN tab of your game, If nothing goes wrong(which hopefully will not go wrong) then you will see your target server in LAN tab, if you can't see then it might be possible that the server you are trying is wrong/incorrect or no minecraft server is hosted on the ip, Double check input parameters (especially port)!
+##### AntiVirus False Positive
+I don't know but you might get antivirus warnings using this module, not sure why but even my windows defender was flagging this as Severe Virus sometimes, and it was random (like different filename of executable or extension), Currently this build doesn't give any virus warnings but it might be possible that your antivirus might flag it as virus! So add this as in quarantine if it happens (it is safe anyways :))
+
+## Bindings
+As i was rushing to finish this, I could'nt prepare it as binding compatible or anything, So if you like to use this script in other languages (python, nodejs etc etc) then you are free to invoke the script with parameters, Or even better! Make your own bindings and do PRs to update this repo! :)
+
+## License
+GPLv3
+
+    Copyright (C) 2021 Harishankar Kumar
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+**Free Software, Hell Yeah!**
---
+++ bedrockProxy.cpp
@@ -0,0 +1,235 @@
+/*
+ * bedrockProxy.c
+ * (C) 2019, all rights reserved,
+ *
+ * This file is part of Vortex Desktop.
+ *
+ */
+
+/*
+ * DESCRIPTION:
+ * Setup a tunnel to expose minecraft servers to LAN in Minecraft
+ * Windows 10 Bedrock Version.
+ *
+ * usage: bedrockProxy.exe [DestIPv4Addr] [DestPort]
+ */
+// Default filter udp.DstPort == 19132
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "windivert.h"
+
+#define ntohs(x)            WinDivertHelperNtohs(x)
+#define ntohl(x)            WinDivertHelperNtohl(x)
+
+#define htons(x)            WinDivertHelperHtons(x)
+#define htonl(x)            WinDivertHelperHtonl(x)
+
+#define MAXBUF              WINDIVERT_MTU_MAX
+#define INET6_ADDRSTRLEN    45
+
+/*
+ * Helper function to convert String to UINT
+ */
+unsigned int convert(char *st) {
+  char *x;
+  for (x = st ; *x ; x++) {
+    if (!isdigit(*x))
+      return 0L;
+  }
+  return (strtoul(st, 0L, 10));
+}
+
+void showGreetingTexts(){
+  printf("--------------\n");
+  printf("Eyy Welcome to Minecraft Bedrock Server Exposer! Here we expose your external servers to LAN tab of Minecraft :D ");
+  printf("Current Version:%s\n", "1.0.0");
+  printf("Author: Agent_Orange#9852 (Discord)\n");
+  printf("This Module Is OpenSourced, Link- https://github.com/hari01584/libWin10MinecraftTunnel\n");
+  printf("--------------\n\n\n");
+}
+
+/*
+ * Entry.
+ */
+int __cdecl main(int argc, char **argv)
+{
+    HANDLE handle, console;
+    UINT i;
+    INT16 priority = 0;
+    unsigned char packet[MAXBUF];
+    UINT packet_len;
+    WINDIVERT_ADDRESS addr;
+    PWINDIVERT_IPHDR ip_header;
+    PWINDIVERT_UDPHDR udp_header;
+    UINT32 src_addr[4], dst_addr[4];
+    UINT32 targetAddr;
+    UINT16 targetPort;
+    UINT64 hash;
+    char src_str[INET6_ADDRSTRLEN+1], dst_str[INET6_ADDRSTRLEN+1];
+    const char *err_str;
+    LARGE_INTEGER base, freq;
+    double time_passed;
+
+    // Check arguments.
+    switch (argc)
+    {
+        case 2:
+            break;
+        case 3:
+            priority = (INT16)atoi(argv[2]);
+            break;
+        default:
+            fprintf(stderr, "usage: %s [DestIPv4Addr] [DestPort]\n",
+                argv[0]);
+            fprintf(stderr, "examples:\n");
+            fprintf(stderr, "\t%s \"149.56.206.101\" 19132\n", argv[0]);
+            exit(EXIT_FAILURE);
+    }
+
+    // Get console for pretty colors.
+  //  console = GetStdHandle(STD_OUTPUT_HANDLE);
+    showGreetingTexts();
+
+    // Sanitize args
+    if(!WinDivertHelperParseIPv4Address(argv[1], &targetAddr)){
+      fprintf(stderr, "error: bad target address(%d) - %s", GetLastError(), argv[1]);
+      return -1;
+    }
+    if((targetPort = convert(argv[2])) == 0L){
+      fprintf(stderr, "error: bad target port(%s)", argv[2]);
+      return -1;
+    }
+    // Divert traffic matching the filter:
+   char filter[80];
+   sprintf(filter, "udp.DstPort == 19132 or udp.DstPort == %s", argv[2]);
+   handle = WinDivertOpen(filter, WINDIVERT_LAYER_NETWORK, priority,
+        WINDIVERT_FLAG_FRAGMENTS);
+    if (handle == INVALID_HANDLE_VALUE)
+    {
+        if (GetLastError() == ERROR_INVALID_PARAMETER &&
+            !WinDivertHelperCompileFilter(filter, WINDIVERT_LAYER_NETWORK,
+                NULL, 0, &err_str, NULL))
+        {
+            fprintf(stderr, "error: invalid filter \"%s\"\n", err_str);
+            exit(EXIT_FAILURE);
+        }
+        fprintf(stderr, "error: failed to open the WinDivert device (%d)\n",
+            GetLastError());
+        exit(EXIT_FAILURE);
+    }
+
+    // Max-out the packet queue:
+    if (!WinDivertSetParam(handle, WINDIVERT_PARAM_QUEUE_LENGTH,
+            WINDIVERT_PARAM_QUEUE_LENGTH_MAX))
+    {
+        fprintf(stderr, "error: failed to set packet queue length (%d)\n",
+            GetLastError());
+        exit(EXIT_FAILURE);
+    }
+    if (!WinDivertSetParam(handle, WINDIVERT_PARAM_QUEUE_TIME,
+            WINDIVERT_PARAM_QUEUE_TIME_MAX))
+    {
+        fprintf(stderr, "error: failed to set packet queue time (%d)\n",
+            GetLastError());
+        exit(EXIT_FAILURE);
+    }
+    if (!WinDivertSetParam(handle, WINDIVERT_PARAM_QUEUE_SIZE,
+            WINDIVERT_PARAM_QUEUE_SIZE_MAX))
+    {
+        fprintf(stderr, "error: failed to set packet queue size (%d)\n",
+            GetLastError());
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up timing:
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&base);
+
+    printf("Starting tool! Go to LAN tab of minecraft bedrock to see your server!!");
+    // Main loop:
+    while (TRUE)
+    {
+        // Read a matching packet.
+        if (!WinDivertRecv(handle, packet, sizeof(packet), &packet_len, &addr))
+        {
+            fprintf(stderr, "warning: failed to read packet (%d)\n",
+                GetLastError());
+            continue;
+        }
+
+        // Print info about the matching packet.
+        WinDivertHelperParsePacket(packet, packet_len, &ip_header, NULL,
+            NULL, NULL, NULL, NULL, &udp_header, NULL,
+            NULL, NULL, NULL);
+        if (ip_header == NULL)
+        {
+            fprintf(stderr, "warning: junk packet\n");
+        }
+
+        // Dump packet info:
+        // putchar('\n');
+        // SetConsoleTextAttribute(console, FOREGROUND_RED);
+        // time_passed = (double)(addr.Timestamp - base.QuadPart) /
+        //     (double)freq.QuadPart;
+        // hash = WinDivertHelperHashPacket(packet, packet_len, 0);
+        // printf("Packet [Timestamp=%.8g, Direction=%s IfIdx=%u SubIfIdx=%u "
+        //     "Loopback=%u Hash=0x%.16llX]\n",
+        //     time_passed, (addr.Outbound?  "outbound": "inbound"),
+        //     addr.Network.IfIdx, addr.Network.SubIfIdx, addr.Loopback, hash);
+        if (ip_header != NULL && udp_header != NULL)
+        {
+            //WinDivertHelperParseIPv4Address("149.56.206.101", &target);
+            //ip_header->DstAddr = htonl(target);
+
+            UINT32 srcAddr = ntohl(ip_header->SrcAddr);
+            UINT32 dstAddr = ntohl(ip_header->DstAddr);
+            WinDivertHelperFormatIPv4Address(srcAddr,
+                src_str, sizeof(src_str));
+            WinDivertHelperFormatIPv4Address(dstAddr,
+                dst_str, sizeof(dst_str));
+
+            // SetConsoleTextAttribute(console,
+            //     FOREGROUND_GREEN | FOREGROUND_RED);
+            // printf("IPv4 [Version=%u HdrLength=%u TOS=%u Length=%u Id=0x%.4X "
+            //     "Reserved=%u DF=%u MF=%u FragOff=%u TTL=%u Protocol=%u "
+            //     "Checksum=0x%.4X SrcAddr=%s DstAddr=%s]\n",
+            //     ip_header->Version, ip_header->HdrLength,
+            //     ntohs(ip_header->TOS), ntohs(ip_header->Length),
+            //     ntohs(ip_header->Id), WINDIVERT_IPHDR_GET_RESERVED(ip_header),
+            //     WINDIVERT_IPHDR_GET_DF(ip_header),
+            //     WINDIVERT_IPHDR_GET_MF(ip_header),
+            //     ntohs(WINDIVERT_IPHDR_GET_FRAGOFF(ip_header)), ip_header->TTL,
+            //     ip_header->Protocol, ntohs(ip_header->Checksum), src_str,
+            //     dst_str);
+
+
+            UINT16 srcPort = ntohs(udp_header->SrcPort);
+            UINT16 dstPort = ntohs(udp_header->DstPort);
+            // printf("UDP [SrcPort=%u DstPort=%u Length=%u "
+            //     "Checksum=0x%.4X]\n",
+            //     srcPort, dstPort,
+            //     ntohs(udp_header->Length), ntohs(udp_header->Checksum));
+
+            if(dstAddr == targetAddr && dstPort == targetPort){
+              //SetConsoleTextAttribute(console, FOREGROUND_GREEN);
+              printf("Successfully Connected!\n");
+              break;
+            }
+            if(dstPort == 19132){
+              ip_header->DstAddr = htonl(targetAddr);
+              udp_header->DstPort = htons(targetPort);
+            }
+        }
+
+        WinDivertHelperCalcChecksums(packet, packet_len, &addr, 0);
+        if (!WinDivertSend(handle, packet, packet_len, NULL, &addr))
+        {
+            fprintf(stderr, "warning: failed to reinject packet (%d)\n",
+                GetLastError());
+        }
+    }
+}
